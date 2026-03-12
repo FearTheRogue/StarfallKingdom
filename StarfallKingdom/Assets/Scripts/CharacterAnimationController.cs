18766497eec1e1a9dfeab2e228044b1ef9d66ae5
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-03-12 17:04:00 +0000

    refactor: create shared character animation controller for player and enemies

---
+++ StarfallKingdom/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,44 @@
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class CharacterAnimationController : MonoBehaviour
+{
+    private static readonly int SpeedHash = Animator.StringToHash("Speed");
+    private static readonly int AttackHash = Animator.StringToHash("Attack");
+    private static readonly int HitHash = Animator.StringToHash("Hit");
+    private static readonly int DeadHash = Animator.StringToHash("Dead");
+    private static readonly int PickupHash = Animator.StringToHash("Pickup");
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetSpeed(float speed)
+    {
+        animator.SetFloat(SpeedHash, speed);
+    }
+
+    public void TriggerAttack()
+    {
+        animator.SetTrigger(AttackHash);
+    }
+
+    public void TriggerHit()
+    {
+        animator.SetTrigger(HitHash);
+    }
+
+    public void SetDead(bool isDead)
+    {
+        animator.SetBool(DeadHash, isDead);
+    }
+
+    public void TriggerPickup()
+    {
+        animator.SetTrigger(PickupHash);
+    }
+}
