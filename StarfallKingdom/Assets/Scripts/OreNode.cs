c136ba6df656fea8ba71ede9fe00633937a1acc1
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-04-03 17:13:14 +0100

    feat: add random spin to mined resource drops

--- StarfallKingdom/Assets/Scripts/OreNode.cs
+++ StarfallKingdom/Assets/Scripts/OreNode.cs
@@ -19,6 +19,10 @@ public class OreNode : MonoBehaviour
     [SerializeField] private float maxLaunchForce = 4f;
     [SerializeField] private float upwardsLaunchBias = 1.2f;
 
+    [Header("Spin")]
+    [SerializeField] private float minSpinForce = 1f;
+    [SerializeField] private float maxSpinForce = 4f;
+
     public ResourceType ResourceType => resourceType;
     public bool IsDepleted => hitsRemaining <= 0f;
 
@@ -45,12 +49,17 @@ public class OreNode : MonoBehaviour
 
         GameObject droppedObject = Instantiate(droppedResourcePrefab, spawnOrigin + randomOffset, Quaternion.identity);
 
-        if (droppedObject.TryGetComponent(out Rigidbody rd))
+        if (droppedObject.TryGetComponent(out Rigidbody rb))
         {
             Vector3 launchDirection = new Vector3(Random.Range(-1f, 1f), upwardsLaunchBias, Random.Range(-1f, 1f)).normalized;
 
             float launchForce = Random.Range(minLaunchForce, maxLaunchForce);
-            rd.AddForce(launchDirection * launchForce, ForceMode.Impulse);
+            rb.AddForce(launchDirection * launchForce, ForceMode.Impulse);
+
+            Vector3 randomSpin = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+
+            float spinForce = Random.Range(minSpinForce, maxSpinForce);
+            rb.AddTorque(randomSpin * spinForce, ForceMode.Impulse);
         }
     }
 
