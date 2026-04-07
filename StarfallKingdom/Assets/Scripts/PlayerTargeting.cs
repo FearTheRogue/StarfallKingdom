0ed209dc7ac00f431620b1b8766fb80edb2dfce1
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-04-07 16:59:27 +0100

    refactor: extract current target handling into player targeting

--- StarfallKingdom/Assets/Scripts/PlayerTargeting.cs
+++ StarfallKingdom/Assets/Scripts/PlayerTargeting.cs
@@ -1,16 +1,49 @@
+using UnityEditor.AdaptivePerformance.Editor;
 using UnityEngine;
+using UnityEngine.Rendering.RenderGraphModule;
 
+[RequireComponent(typeof(PlayerEffects))]
 public class PlayerTargeting : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerEffects effects;
+    private Interactable currentTarget;
+
+    private void Awake()
     {
-        
+        effects = GetComponent<PlayerEffects>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Interactable CurrentTarget => currentTarget;
+
+    public void HandleClickResult(Interactable interactable)
     {
-        
+        if (interactable != null)
+        {
+            SetTarget(interactable);
+            return;
+        }
+
+        ClearTarget();
+    }
+
+    public void SetTarget(Interactable interactable)
+    {
+        currentTarget = interactable;
+
+        if (currentTarget != null)
+        {
+            effects.ShowTargetIndicator(currentTarget.transform);
+        }
+    }
+
+    public void ClearTarget()
+    {
+        currentTarget = null;
+        effects.HideTargetIndicator();
+    }
+
+    public bool HasValidTarget()
+    {
+        return currentTarget != null && currentTarget.gameObject != null;
     }
 }
