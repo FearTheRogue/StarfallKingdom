9406d571439463b49d68a5ca89e444c797ee6d69
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-04-08 17:42:49 +0100

    feat: add inventory item data definition

---
+++ StarfallKingdom/Assets/Scripts/InventoryItemData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public enum ItemCategory
+{
+    Material,
+    Tool,
+    Quest,
+    Valuable
+}
+
+[CreateAssetMenu(fileName = "NewInventoryItem", menuName = "Inventory/Item Data")]
+public class InventoryItemData : ScriptableObject
+{
+    [Header("Item Into")]
+    [SerializeField] private string itemId;
+    [SerializeField] private string itemName;
+    [SerializeField] private Sprite icon;
+    [SerializeField] private ItemCategory category;
+
+    public string ItemId => itemId;
+    public string ItemName => itemName;
+    public Sprite Icon => icon;
+    public ItemCategory Category => category;
+}
