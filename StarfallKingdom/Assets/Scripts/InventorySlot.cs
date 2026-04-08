92b4df38819007963a2558acb78ded0ecd267a89
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-04-08 17:44:06 +0100

    feat: add runtime inventory slot model

---
+++ StarfallKingdom/Assets/Scripts/InventorySlot.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.Contracts;
+using UnityEngine;
+
+[System.Serializable]
+public class InventorySlot : MonoBehaviour
+{
+    public InventoryItemData itemData;
+    public int quantity;
+
+    public InventorySlot(InventoryItemData itemData, int quantity)
+    {
+        this.itemData = itemData;
+        this.quantity = quantity;
+    }
+}
